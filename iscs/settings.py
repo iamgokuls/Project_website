50ce953d3cfb0483fc24ad66a6d75d14f4115cda
iamgokuls <31001690+iamgokuls@users.noreply.github.com>
2020-07-30 10:31:54 +0530

    Update settings.py

--- iscs/settings.py
+++ iscs/settings.py
@@ -124,6 +124,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' # mail service smtp
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gars.system@gmail.com' # email id
-EMAIL_HOST_PASSWORD = 'gars@123' #password
+EMAIL_HOST_USER = '' # email id
+EMAIL_HOST_PASSWORD = '' #password
 
